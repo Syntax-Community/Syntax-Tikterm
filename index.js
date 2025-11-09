a5b3e37d8d23be288d3b8ac3f242b106d155b70b
SyntaxComunity <achmadyludya01@gmail.com>
2025-11-09 10:33:28 +0700

    dont forget to "npm i"

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 import readlineSync from "readline-sync";
+import dotenv from 'dotenv';
+dotenv.config();
 import { WebcastPushConnection } from "tiktok-live-connector";
 const username = readlineSync.question("Masukkan username TikTok (tanpa @): ");
-const tiktok = new WebcastPushConnection(username);
+const tiktok = new WebcastPushConnection(username, {
+  enableWebsocket: true,
+  clientParams: { apiKey: process.env.APIKEY }
+});
 tiktok.connect()
   .then(() => {
     console.log("✅ terhubung ke " + username);
@@ -16,8 +21,12 @@ tiktok.on("chat", (data) => {
   console.log(`[💬] \x1b[31m${data.nickname}\x1b[0m: ` + `\x1b[36m${data.comment}\x1b[0m`);
 });
 tiktok.on("social", (data)=> {
-  console.log(data);
+  console.log(`[❗️]\x1b[95m`, data.nickname, "followed the host\x1b[0m");
 });
 tiktok.on("gift", (data)=> {
-  console.log(`🎁 ${data.nickname} has sent gift ${data.giftName} x${data.repeatCount}`);
+   if (data.giftType === 1 && !data.repeatEnd) {
+    console.log(`🎁 ${data.nickname} has sent gift ${data.giftName} x${data.repeatCount}`);
+  } else {
+     console.log(`🎁 ${data.nickname} has sent gift ${data.giftName} x${data.repeatCount}`);
+   }
 });
---
+++ readme.md
@@ -0,0 +1,6 @@
+# SETUP
+```npm i
+- login to https://www.eulerstream.com
+- get your api key
+- setup .env "APIKEY=<type here>"
+``` node .
